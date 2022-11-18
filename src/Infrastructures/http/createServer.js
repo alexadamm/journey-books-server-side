7922f14e9b187d0452d1347deb0bbc7565d7b1ee
Alexander Adam <alexanderadamm21@gmail.com>
2022-11-18 16:51:07 +0700

    Allow CORS for all origins

--- src/Infrastructures/http/createServer.js
+++ src/Infrastructures/http/createServer.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const { default: helmet } = require('helmet');
 
 const users = require('../../Interfaces/http/api/users/routes');
@@ -13,6 +14,7 @@ const createServer = async (container) => {
 
   const middlewares = new ServerMiddlewares(container);
 
+  app.use(cors());
   app.use(express.json());
   app.use(helmet());
   app.use(express.urlencoded({ extended: true }));
